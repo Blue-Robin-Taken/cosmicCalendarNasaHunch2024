ddfed261092908171cb3c6368ce6f25a91375116
Blue-Robin-Taken <julianjohnt@gmail.com>
2024-10-29 00:58:21 -0500

    fix tomorrow );

--- src/app/login/components/actions.tsx
+++ src/app/login/components/actions.tsx
@@ -1,21 +1,34 @@
 "use server"; // DO NOT REMOVE, it resolves to client. (endpoints must be secured here as well)
 import { db } from "@/app/database";
+import { error } from "console";
 import { SignJWT } from "jose";
+import { cookies } from "next/headers";
 
 export default async function loginServerAction(formData: FormData) {
+  async function giveToken(user: string) {
+    const token = await new SignJWT({
+      username: user,
+    })
+      .setProtectedHeader({ alg: "HS256" })
+      .setIssuedAt()
+      .setExpirationTime("2h")
+      .sign(new TextEncoder().encode(process.env.JWT_KEY_SIGNATURE));
+
+    cookies().set("token", token);
+    return Response.json({ loggedIn: true }); // For the redirect
+  }
+
   const rawFormData = {
     username: formData.get("username"),
     password: formData.get("password"),
   };
+  console.log(rawFormData.password?.toString());
   const getDB = db.get(
-    "SELECT * FROM users WHERE username = ?",
-    [rawFormData.username?.toString()],
-    (err, row: { username: string; password: string }) => {
-      if (row.password) {
-        console.log(row.password, rawFormData.username?.toString());
-        if (row.password == rawFormData.password?.toString()) {
-          console.log("yipppee");
-        }
+    "SELECT * FROM users WHERE username = ? AND password = ?",
+    [rawFormData.username?.toString(), rawFormData.password?.toString()],
+    async (err, row: { username: string; password: string }) => {
+      if (row != null) {
+        giveToken(row.username);
       }
     }
   );
