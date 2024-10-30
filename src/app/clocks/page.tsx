679ab6bc645382742fd4e1ac5107fb963c775dad
Blue-Robin-Taken <julianjohnt@gmail.com>
2024-10-30 01:26:07 -0500

    server side GMT clocks (don't update just yet though)

--- src/app/clocks/page.tsx
+++ src/app/clocks/page.tsx
@@ -1,21 +1,59 @@
+import date from "date-and-time";
+import { setInterval } from "timers/promises";
 export default function Clocks() {
   // Generate all GMT Divs
+
   const divList = [];
   for (let x = -12; x <= 12; x++) {
     divList.push(x);
   }
   const mapped = divList.map((number, index) => {
+    function returnNum(n: number) {
+      // basically just returns the num with a "+" for positive or nothing for negative, it just looks better
+      if (n > 0) {
+        if (n < 10) {
+          return `+0${n}:00`;
+        }
+        if (n >= 10) {
+          return `+${n}:00`;
+        }
+      } else {
+        if (n == 0) {
+          return "00:00";
+        }
+        if (n >= 10) {
+          return `-0${n * -1}:00`;
+        } else {
+          return `${n}:00`;
+        }
+      }
+    }
+
+    let d = new Date(Date.now());
+    d = date.addHours(d, number);
+
     return (
-      <div key={index} className="w-10 h-10 rounded-sm bg-slate-600">
-        {number}
+      // For the GMT divs
+
+      <div
+        key={index}
+        className="w-60 h-60 rounded-lg bg-slate-600 text-center"
+      >
+        <h1 className="justify-center text-lg font-semibold m-3">
+          GMT {returnNum(number)}
+        </h1>
+        <h1 className="justify-center text-base font-medium m-2">
+          {date.format(d, `hh:mm A [GMT]${number}`, true)}
+        </h1>
       </div>
     );
   });
 
   return (
+    // Main return
     <>
       <div>
-        <div className="flex flex-row flex-grow row-auto space-x-11 ">
+        <div className="grid grid-cols-4 grid-flow-row min-h-screen gap-y-10 gap-x-10 m-5 justify-items-center">
           {mapped}
         </div>
       </div>
