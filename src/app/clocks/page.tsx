b5382bd14867eae416ee50cac91ed71130dbd836
Blue-Robin-Taken <julianjohnt@gmail.com>
2024-10-29 23:12:30 -0500

    fixed warning in clocks page

--- src/app/clocks/page.tsx
+++ src/app/clocks/page.tsx
@@ -1,16 +1,16 @@
-export default function Clocks(){
-    // Generate all GMT Divs
-    const divList = []
-    for (let x=-12; x<=12; x++){
-        divList.push(x)
-    }
-    const mapped = divList.map((number,index)=>{
-        return (<><div key={number}>{number}</div></>)
-    })
+export default function Clocks() {
+  // Generate all GMT Divs
+  const divList = [];
+  for (let x = -12; x <= 12; x++) {
+    divList.push(x);
+  }
+  const mapped = divList.map((number, index) => {
+    return <div key={index}>{number}</div>;
+  });
 
-    return (<>
-    <div key="holder">
-        {mapped}
-    </div>
-    </>)
+  return (
+    <>
+      <div key="holder">{mapped}</div>
+    </>
+  );
 }
--- src/app/login/components/loginForm.tsx
+++ src/app/login/components/loginForm.tsx
@@ -15,21 +15,36 @@ export default function LoginForm() {
   }
   return (
     <>
-      <form action={loginSubmit}>
-        <label>
-          Username
-          <input type="text" name="username" />
-        </label>
-        <label htmlFor="password">
-          Password
-          <input type="text" name="password"></input>
-        </label>
-        <button type="submit">Login</button>
-      </form>
+      <div className="flex flex-grow flex-col justify-center align-center min-h-screen">
+        <h1 className="text-center text-5xl">Log In</h1>
+        <form
+          action={loginSubmit}
+          className="m-5 p-5 flex place-items-center flex-col justify-center"
+        >
+          <input
+            type="text"
+            name="username"
+            placeholder="username"
+            className="text-black p-3 m-3 text-lg"
+          />
+          <input
+            type="text"
+            name="password"
+            placeholder="password"
+            className="text-black p-3 m-3 text-lg"
+          ></input>
+          <button
+            type="submit"
+            className="bg-slate-600 rounded-lg m-2 p-3 w-32"
+          >
+            Log In
+          </button>
+        </form>
 
-      {showInvalid && (
-        <p className="justify-center">Incorrect password or username</p>
-      )}
+        {showInvalid && (
+          <p className="justify-center">Incorrect password or username</p>
+        )}
+      </div>
     </>
   );
 }
