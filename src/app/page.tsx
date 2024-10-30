a5f78de8cceacccd1eab2a23921e070add2e5090
Julian Thomas <julianjohnt@gmail.com>
2024-10-29 20:06:32 -0500

    centered background!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex justify-center fixed h-screen w-screen bg-white">
-      <div className="flex items-center justify-center flex-col wrapper"> 
-              <h1 className="text-9xl font-bold font-Cinzel mb-4 text-center bg-[url('./images/cosmicYellowBackground.png')] bg-clip-text text-transparent">
+      <div className="-z-10 flex flex-grow align-middle h-screen w-screen top-0 left-0 fixed items-center justify-center flex-col wrapper  bg-white"> 
+              <h1 className="align-middle text-9xl font-bold font-Cinzel mb-4 text-center bg-[url('./images/cosmicYellowBackground.png')] bg-clip-text text-transparent">
               TerraChronos 
               </h1> 
-          <h2 className="align-center font-CinzelDecorative justify-center text-3xl text-black">
+          <h2 className="align-middle font-CinzelDecorative justify-center text-3xl text-black">
           A Cosmic Calendar for all your Dates!
           </h2>
       </div>
-    </div>
   );
 }
