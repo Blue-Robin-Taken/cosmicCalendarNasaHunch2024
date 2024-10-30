599208f68546073fa48fcf6a9899d657b0fe1464
xiberxia <tyhjdragon@gmail.com>
2024-10-29 23:34:19 -0500

    made clock update per second or so

--- src/app/clock/page.tsx
+++ src/app/clock/page.tsx
@@ -1,24 +1,41 @@
+'use client';
 import { DateTime } from "@tubular/time"
 import ttime from "@tubular/time"
 import React from "react";
+import { useState, useEffect } from "react";
+
 
 const timeNow = ttime().toString()
 // DateTime<yyyy-mm-ddThh:mm:ss.iii-0:00§>
 // y=year ; m=month ; d=date ; T ; h=hour ; m=minute ; s=second ; i=milliseconds ; UTC deviation
 
-function GetTime() {
-    const timeNow = ttime()
-    const msNow = timeNow.format('x')
+// function GetTime() {
+//     const [time, setTime] = useState(ttime())
 
+//     useEffect(() => {
+//         const interval = setInterval(() => {
+//             setTime(ttime());
+//         }, 1000);
 
-    return timeNow;
-}
+//         return () => clearInterval(interval);
+//     }, [])
 
-setInterval(GetTime, 1000)
+//     return time;
+    
+// }
+
+//setInterval(GetTime, 5)
 
 export default function Clock() {
-    return <div>
-        <p>{timeNow}</p>
+    const [time, setTime] = useState(ttime())
 
-    </div>
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(ttime());
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [])
+
+    return <p>{time.toString()}</p>;
 }
