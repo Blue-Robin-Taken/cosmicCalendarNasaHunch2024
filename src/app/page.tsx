5f8e649846834d7196e4c8d8a475494ba65a5bdb
Blue-Robin-Taken <julianjohnt@gmail.com>
2024-11-25 23:47:09 -0600

    added text thingamamonb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,75 @@
-const tempName = "TerraChronos";
-import "./about/page.css";
+const tempName = 'TerraChronos';
+import './about/page.css';
 
 export default function Home() {
-  return (
-    // https://stackoverflow.com/a/76827457/15982771
-    <div className="mt-40 flex w-auto items-center justify-center flex-col wrapper select-none overflow-x-auto ">
-      <h1 className="align-middle text-9xl font-bold font-Cinzel mb-4 text-center bg-[url('./images/cosmicYellowBackground.png')] bg-clip-text text-transparent">
-        TerraChronos
-      </h1>
-      <h2 className="mb-40 align-middle font-CinzelDecorative  justify-center text-3xl text-black">
-        A Cosmic Calendar for all your Dates!
-      </h2>
-      
-      {/* About section */}
-      <div className="mb-16 selection:bg-highlight-yellow">
-      <div className="flex items-center justify-center flex-col wrapper bg-white">
-        <div className="bg-gray-200 p-8 rounded-lg shadow-lg max-w-2xl">
-          <h1 className="text-4xl font-bold font-Cinzel mb-4 text-center text-theme-yellowlight">
-            {tempName}
-          </h1>
-          <h3 className="text-black text-lg font-CinzelDecorative text-center mb-4">
-            Helps you keep track of time no matter where you&apos;re at.
-          </h3>
-          <p className="text-black mb-4 text-center">
-            By standardizing time-keeping with a single seamless stream of
-            software, {tempName} empowers communication and collaboration
-            between interplanetary citizens. {tempName} synchronizes all time
-            zones between all celestial bodies, whether you&apos;re on one of
-            Earth&apos;s 24 zones, across the vermillion stretches of Mars, or
-            at the far horizons of Ceres. Any starry traveler or meticulous
-            researcher will find stability by using {tempName} as their
-            timekeeper.{" "}
-          </p>
-        </div>
+    return (
+        // https://stackoverflow.com/a/76827457/15982771
+        <div className="dark:bg-black mt-40 flex w-auto items-center justify-center flex-col wrapper select-none overflow-x-auto ">
+            <h1 className="align-middle text-9xl font-bold font-Cinzel mb-4 text-center bg-[url('./images/cosmicYellowBackground.png')] bg-clip-text text-transparent">
+                TerraChronos
+            </h1>
+            <h2 className="mb-40 align-middle font-CinzelDecorative  justify-center text-3xl text-black">
+                A Cosmic Calendar for all your Dates!
+            </h2>
 
-        <div className="slide ml-10 mt-16">
-          <h2 className="text-2xl font-bold font-Cinzel mb-4 text-theme-yellowlight">
-            Features:
-          </h2>
-          <ul className="text-black list-disc">
-            <li>
-              Save all your personal settings and data with our Log-in system
-            </li>
-            <li>
-              Access all your tools with a single swish and click on the
-              Navigation Bar
-            </li>
-            <li>
-              Fine tune your interstellar experience in the Settings interface
-            </li>
-            <li>Select your location on any planet, moon, or star</li>
-            <li>
-              Organize your time with the Calendar function that transcends the
-              boundaries of space
-            </li>
-            <li>
-              Discover a list of every clock you might fancy with the Clock page
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+            {/* About section */}
+            <div className="mb-16 selection:bg-highlight-yellow">
+                <div className="flex items-center justify-center flex-col wrapper bg-white">
+                    <div className="bg-gray-200 p-8 rounded-lg shadow-lg max-w-2xl">
+                        <h1 className="text-4xl font-bold font-Cinzel mb-4 text-center text-theme-yellowlight">
+                            {tempName}
+                        </h1>
+                        <h3 className="text-black text-lg font-CinzelDecorative text-center mb-4">
+                            Helps you keep track of time no matter where
+                            you&apos;re at.
+                        </h3>
+                        <p className="text-black mb-4 text-center">
+                            By standardizing time-keeping with a single seamless
+                            stream of software, {tempName} empowers
+                            communication and collaboration between
+                            interplanetary citizens. {tempName} synchronizes all
+                            time zones between all celestial bodies, whether
+                            you&apos;re on one of Earth&apos;s 24 zones, across
+                            the vermillion stretches of Mars, or at the far
+                            horizons of Ceres. Any starry traveler or meticulous
+                            researcher will find stability by using {tempName}{' '}
+                            as their timekeeper.{' '}
+                        </p>
+                    </div>
 
-    </div>
-  );
+                    <div className="slide ml-10 mt-16">
+                        <h2 className="text-2xl font-bold font-Cinzel mb-4 text-theme-yellowlight">
+                            Features:
+                        </h2>
+                        <ul className="text-black list-disc">
+                            <li>
+                                Save all your personal settings and data with
+                                our Log-in system
+                            </li>
+                            <li>
+                                Access all your tools with a single swish and
+                                click on the Navigation Bar
+                            </li>
+                            <li>
+                                Fine tune your interstellar experience in the
+                                Settings interface
+                            </li>
+                            <li>
+                                Select your location on any planet, moon, or
+                                star
+                            </li>
+                            <li>
+                                Organize your time with the Calendar function
+                                that transcends the boundaries of space
+                            </li>
+                            <li>
+                                Discover a list of every clock you might fancy
+                                with the Clock page
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,51 +1,49 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    
-    extend: {
-      
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        'navbar-text': '#575757',
-        'navbar-tctext': '#301B02',
-        'theme-yellowlight': '#E7BA09',
-        'theme-yellowdark': '#CA9503',
-        'theme-graylight': '#D9D9D9',
-        'highlight-yellow': '#ffe770',
-      },
-      fontFamily: {
-        sans: [
-          'Lucida Sans',
-          'Lucida Sans Regular',
-          'Lucida Grande',
-          'Lucida Sans Unicode',
-          'Geneva',
-          'Verdana',
-          'sans-serif',
-        ],
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: 'var(--background)',
+                foreground: 'var(--foreground)',
+                'navbar-text': '#575757',
+                'navbar-tctext': '#301B02',
+                'theme-yellowlight': '#E7BA09',
+                'theme-yellowdark': '#CA9503',
+                'theme-graylight': '#D9D9D9',
+                'highlight-yellow': '#ffe770',
+                'darkmode-bg': '#202020',
+                'darkmode-main': '#DAAE01',
+                'darkmode-shadow': '#CA9503',
+                'darkmode-text-yellowlight': '#FFF9E3',
+                'darkmode-text-lightpink': '#FFE8E8',
+                'darkmode-text-lightblue': '#E4E4E4',
+                'darkmode-box-darklight': '#323232',
+            },
+            fontFamily: {
+                sans: [
+                    'Lucida Sans',
+                    'Lucida Sans Regular',
+                    'Lucida Grande',
+                    'Lucida Sans Unicode',
+                    'Geneva',
+                    'Verdana',
+                    'sans-serif',
+                ],
 
-        Cinzel: [
-            'Cinzel'
-        ],
+                Cinzel: ['Cinzel'],
 
-        CinzelDecorative: [
-          'Cinzel-Decorative'
-        ],
+                CinzelDecorative: ['Cinzel-Decorative'],
 
-        PublicSans: [
-          'Public-Sans'
-        ]
-  
-      },
+                PublicSans: ['Public-Sans'],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
