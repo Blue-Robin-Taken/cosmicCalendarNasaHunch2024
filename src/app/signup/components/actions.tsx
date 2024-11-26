bcfd4205b88ae45a5a480cc5987ffe60d39d12c8
Blue-Robin-Taken <julianjohnt@gmail.com>
2024-11-25 23:15:38 -0600

    fixed bug where user would still get added even if their password wasn't long enough

--- src/app/signup/components/actions.tsx
+++ src/app/signup/components/actions.tsx
@@ -1,53 +1,54 @@
-"use server"; // DO NOT REMOVE, it resolves to client (because there is a function from here that's ran in a client func). All endpoints here MUST be secured.
-import { db } from "@/app/database";
+'use server'; // DO NOT REMOVE, it resolves to client (because there is a function from here that's ran in a client func). All endpoints here MUST be secured.
+import { db } from '@/app/database';
 
 export default async function serverSignup(formData: FormData): Promise<{
-  formLengthInvalid: boolean;
-  userExists: boolean;
-}> {
-  const returnData: {
     formLengthInvalid: boolean;
     userExists: boolean;
-  } = {
-    formLengthInvalid: false,
-    userExists: false,
-  };
-  const rawFormData: {
-    username: FormDataEntryValue | null;
-    password: FormDataEntryValue | null;
-  } = {
-    username: formData.get("username"),
-    password: formData.get("password"),
-  };
+}> {
+    const returnData: {
+        formLengthInvalid: boolean;
+        userExists: boolean;
+    } = {
+        formLengthInvalid: false,
+        userExists: false,
+    };
+    const rawFormData: {
+        username: FormDataEntryValue | null;
+        password: FormDataEntryValue | null;
+    } = {
+        username: formData.get('username'),
+        password: formData.get('password'),
+    };
 
-  if (rawFormData.username == null) {
-    rawFormData.username = "";
-  }
-  if (rawFormData.password == null) {
-    rawFormData.password = "";
-  }
-
-  if (rawFormData.password.toString().length < 6) {
-    // Length of password set is too little
-    returnData.formLengthInvalid = true;
-  }
-
-  db.get(
-    "SELECT username FROM users WHERE username IS ?",
-    rawFormData.username,
-    (err, row) => {
-      console.log(err, row);
-      if (row == undefined) {
-        db.run(
-          "INSERT INTO users (username, password, email) VALUES(?, ?, ?)",
-          [rawFormData.username, rawFormData.password, null]
-        );
-      } else {
-        returnData.userExists = true;
-      }
+    if (rawFormData.username == null) {
+        rawFormData.username = '';
     }
-  );
+    if (rawFormData.password == null) {
+        rawFormData.password = '';
+    }
 
-  console.log(rawFormData);
-  return returnData;
+    if (rawFormData.password.toString().length < 6) {
+        // Length of password set is too little
+        returnData.formLengthInvalid = true;
+        return returnData;
+    }
+
+    db.get(
+        'SELECT username FROM users WHERE username IS ?',
+        rawFormData.username,
+        (err, row) => {
+            console.log(err, row);
+            if (row == undefined) {
+                db.run(
+                    'INSERT INTO users (username, password, email) VALUES(?, ?, ?)',
+                    [rawFormData.username, rawFormData.password, null]
+                );
+            } else {
+                returnData.userExists = true;
+            }
+        }
+    );
+
+    console.log(rawFormData);
+    return returnData;
 }
