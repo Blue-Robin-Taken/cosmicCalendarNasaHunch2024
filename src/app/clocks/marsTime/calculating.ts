70cae0ee483042c06ac1374fdadaea09675a610a
BlueRobin <julianjohnt@Gmail.com>
2025-01-08 14:25:27 -0600

    it should work lmao hehehehehehhe

--- src/app/clocks/marsTime/calculating.ts
+++ src/app/clocks/marsTime/calculating.ts
@@ -7,8 +7,16 @@ export default function calculation(e: number) {
     3. Add 2440587.5 to the divided epoch time to get the Julian date based of off the UTC time
     4. Add the difference of TAI - UTC which is 37 seconds
     5. Add 32.184 (TAI time difference from Terrestrial Time)
-    6. The above gets you 
+    6. The above gets yout the JDtt time (Julian date Terestrial Time) which you can convert to the difference of time
+    from January 1st 12:00 PM 2000 by subtracting 2451545
+    7. Get January 6th 2000
     */
-    return e / ((8.64 * 10) ^ 7) + 2440587.5 + (32.184 + 32) / 84000;
+    var JDut = (e / (8.64 * 10 ^ 7)) + 2440587.5);
+    var JDtt = JDut + ((32.184 + 37) / 84000))
+    var j2000 = JDtt - 2451545 
+    var MSG = ((j2000 - 4.5) / 1.027491252) + 44796 - 0.00096
+    // Mars standard gTime (gTime = time, this is purely used for meme purposes, do not change varaible or it will break because I will break it)
+    // https://www.giss.nasa.gov/tools/mars24/help/algorithm.html
+    return MSG;
 } // Thank you James Tauber!!!!!!!!!!!!!!!!!!!!
 // https://github.com/jtauber/mars-clock?tab=readme-ov-file
