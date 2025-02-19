552baa9f38e6461aae975aecd5128dd00d4594ba
Blue-Robin-Taken <julianjohnt@gmail.com>
2025-02-18 22:29:14 -0600

    formatting

--- src/app/articles/[article]/page.tsx
+++ src/app/articles/[article]/page.tsx
@@ -12,7 +12,9 @@ export default async function Page({
     var articleJSON = JSON.parse(
         fs.readFileSync('./src/app/articles/blogs.json', 'utf8')
     );
-    if (!articleJSON.Blogs.find((article) => article.title == articleParam)) {
+    if (
+        !articleJSON.Blogs.find((article: any) => article.title == articleParam)
+    ) {
         console.log(articleJSON, articleParam);
         return (
             <>
@@ -25,14 +27,28 @@ export default async function Page({
         `./src/app/articles/files/${articleParam}.md`,
         'utf-8'
     );
+    const article = articleJSON.Blogs.find(
+        (article: any) => article.title == articleParam
+    );
     return (
         <>
-            <div
-                className=""
-                dangerouslySetInnerHTML={{
-                    __html: marked(markdownFile),
-                }}
-            ></div>
+            <div className="flex flex-col items-center justify-center">
+                <h1
+                    className="text-4xl font-bold font-Chocolate mb-4 text-center text-lm-yellow-hl 
+                            dark:text-dm-yellow-hl"
+                >
+                    Title: {article.title}
+                </h1>
+                <h1 className="text-lm-p-text dark:text-white font-Lato mb-4 text-center">
+                    Author: {article.author}
+                </h1>
+                <div
+                    className="text-white text-xl m-3 p-2"
+                    dangerouslySetInnerHTML={{
+                        __html: marked(markdownFile),
+                    }}
+                ></div>
+            </div>
         </>
     );
 }
