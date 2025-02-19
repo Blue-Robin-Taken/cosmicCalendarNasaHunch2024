305c16fa4647a67ad1a3a776fd35e6f2a27072ca
Blue-Robin-Taken <julianjohnt@gmail.com>
2025-02-18 21:17:30 -0600

    got json loading working!!! :D

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,12 +1,40 @@
 export default function articles() {
+    var fs = require('fs');
+
+    var articleJSON = JSON.parse(
+        fs.readFileSync('./src/app/articles/blogs.json', 'utf8')
+    );
     return (
         <>
-            <div className="flex flex-row p-3 m-1 justify-center ">
-                <h1 className="">Welcome to our blog!!!</h1>
+            <div className="flex flex-col p-3 m-1 justify-center items-center">
+                <h1
+                    className="m-10 text-4xl font-bold font-Chocolate mb-4 text-center text-lm-yellow-hl 
+                            dark:text-dm-yellow-hl"
+                >
+                    Welcome to our blog!!!
+                </h1>
 
-                <i>Most of the following are written </i>
+                <i className="m-2 p-10 font-Lato text-3xl flex flex-col justify-center items-start text-lm-h1-text dark:text-dm-h1-text ps-16">
+                    The following are written by the TerraChronos Team
+                </i>
 
                 <h2>Below are some of the blogs:</h2>
+                <div>
+                    {articleJSON.Blogs.map(
+                        (
+                            article: any // Note: remove any later or not hehe
+                        ) => (
+                            <div id={article.title}>
+                                <h3>
+                                    <a href={`./articles/${article.title}`}>
+                                        {article.title}
+                                    </a>
+                                </h3>
+                                <p>By: {article.author}</p>
+                            </div>
+                        )
+                    )}
+                </div>
             </div>
         </>
     );
