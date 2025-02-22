34fcef5e7c1ea7eda84332065dd9e025dc6f271e
Blue-Robin-Taken <julianjohnt@gmail.com>
2025-02-22 13:29:25 -0600

    some formatting

--- src/app/articles/[article]/page.tsx
+++ src/app/articles/[article]/page.tsx
@@ -45,7 +45,7 @@ export default async function Page({
                 <h1 className="text-lm-p-text dark:text-white font-Lato mb-4 text-center">
                     Author: {article.author}
                 </h1>
-                <div className="dark:text-white m-5 p-3">
+                <div className="flex flex-col font-Lato dark:text-white m-5 p-3 [&_p]:m-5 [&_p]:p-3 [&_p]:text-xl [&_h2]:text-5xl [&_h2]:p-3 [&_h2]:m-5 [&_h3]:text-3xl [&_h3]:p-2 [&_h3]:m-4 text-center justify-center [&>span]:flex [&>span]:flex-col [&>span]:p-3 w-3/4">
                     <Latex>{marked(markdownFile).toString()}</Latex>
                 </div>
             </div>
