2fea7c94c466c08b69e33af2c8a43d892a4de785
Julian Thomas <julianjohnt@gmail.com>
2025-02-21 19:38:47 -0600

    YIPEEEEEEEEEE WE GOT MARKDOWWWENNN!!! WOOHOHOHOOOO AND LATEX TOOOO!!!!

--- src/app/articles/[article]/page.tsx
+++ src/app/articles/[article]/page.tsx
@@ -1,60 +1,55 @@
-import { url } from 'inspector';
-import { marked } from 'marked';
-import { parse, HtmlGenerator } from 'latex.js';
+import { url } from "inspector";
+import "katex/dist/katex.min.css";
+import Latex from "react-latex-next";
+import { marked } from "marked";
 
 export default async function Page({
-    params,
+  params,
 }: {
-    params: Promise<{ article: string }>;
+  params: Promise<{ article: string }>;
 }) {
-    const articleParam = decodeURI((await params).article);
-    var fs = require('fs');
+  const articleParam = decodeURI((await params).article);
+  var fs = require("fs");
 
-    var articleJSON = JSON.parse(
-        fs.readFileSync('./src/app/articles/blogs.json', 'utf8')
-    );
-    if (
-        !articleJSON.Blogs.find((article: any) => article.title == articleParam)
-    ) {
-        console.log(articleJSON, articleParam);
-        return (
-            <>
-                <h1>Article Not Found!</h1>
-            </>
-        );
-    }
+  var articleJSON = JSON.parse(
+    fs.readFileSync("./src/app/articles/blogs.json", "utf8")
+  );
 
-    const markdownFile = fs.readFileSync(
-        `./src/app/articles/files/${articleParam}.md`,
-        'utf-8'
-    );
-    const article = articleJSON.Blogs.find(
-        (article: any) => article.title == articleParam
-    );
-    let generator = new HtmlGenerator({ hyphenate: false });
+  if (
+    !articleJSON.Blogs.find((article: any) => article.title == articleParam)
+  ) {
+    console.log(articleJSON, articleParam);
     return (
-        <>
-            <div className="flex flex-col items-center justify-center">
-                <h1
-                    className="text-4xl font-bold font-Chocolate mb-4 text-center text-lm-yellow-hl 
-                            dark:text-dm-yellow-hl"
-                >
-                    {article.title}
-                </h1>
-                <h1 className="text-lm-p-text dark:text-white font-Lato mb-4 text-center">
-                    Author: {article.author}
-                </h1>
-                <div
-                    className="text-white text-xl m-3 p-2"
-                    dangerouslySetInnerHTML={{
-                        __html: marked(
-                            parse(markdownFile, {
-                                generator: generator,
-                            })
-                        ),
-                    }}
-                ></div>
-            </div>
-        </>
+      <>
+        <h1>Article Not Found!</h1>
+      </>
     );
+  }
+
+  const markdownFile = fs.readFileSync(
+    `./src/app/articles/files/${articleParam}.md`,
+    "utf-8"
+  );
+  const article = articleJSON.Blogs.find(
+    (article: any) => article.title == articleParam
+  );
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <h1
+          className="text-4xl font-bold font-Chocolate mb-4 text-center text-lm-yellow-hl 
+                            dark:text-dm-yellow-hl"
+        >
+          {article.title}
+        </h1>
+        <h1 className="text-lm-p-text dark:text-white font-Lato mb-4 text-center">
+          Author: {article.author}
+        </h1>
+        <div className="text-white">
+          <Latex>{marked(markdownFile).toString()}</Latex>
+        </div>
+      </div>
+    </>
+  );
 }
