40d3c0bd7d1892b705382ac0a918a4a0f8a845cc
Blue-Robin-Taken <julianjohnt@gmail.com>
2025-02-22 19:11:27 -0600

    finished darkmode!!!

--- src/app/components/darkmodebutton.tsx
+++ src/app/components/darkmodebutton.tsx
@@ -2,7 +2,10 @@
 import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
-import darkmodeimage from '@/app/images/darkmode&lightmode/darkmode.png';
+import darkmodeimage from '@/app/images/darkmode&lightmode/lightmode.png';
+import lightmodeimage from '@/app/images/darkmode&lightmode/darkmode.png';
+
+const imageSize = 40;
 
 export default function DarkmodeButton() {
     const [mounted, setMounted] = useState(false);
@@ -20,15 +23,31 @@ export default function DarkmodeButton() {
     if (theme === 'dark') {
         return (
             <>
-                <button className="dark:text-dm-lightyellow">
+                <button className="dark:text-dm-lightyellow align-middle">
                     <Image
                         src={darkmodeimage.src}
                         onClick={() => {
                             setTheme('light');
                         }}
                         alt="DarkmodeImage"
-                        width={50}
-                        height={50}
+                        width={imageSize}
+                        height={imageSize}
+                    />
+                </button>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <button className="dark:text-dm-lightyellow align-middle">
+                    <Image
+                        src={lightmodeimage.src}
+                        onClick={() => {
+                            setTheme('dark');
+                        }}
+                        alt="LightmodeImage"
+                        width={imageSize}
+                        height={imageSize}
                     />
                 </button>
             </>
